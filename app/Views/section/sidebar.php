0b498fac4257bd7c64e162911f41c9e07e10e104
unknown <zhebom@gmail.com>
2022-12-10 15:57:52 +0700

    kok imagenya gak ikut ya

--- app/Views/section/sidebar.php
+++ app/Views/section/sidebar.php
@@ -3,7 +3,7 @@
 
 <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="dist/logo/ups.png" alt="ups" height="60" width="60">
         </div>
  <!-- Navbar -->
  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -49,7 +49,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="<?= base_url(); ?>" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AKD FEB UPS</span>
 
