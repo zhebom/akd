996e2a99d52fa334cfeb2441d756e91d41f92909
unknown <zhebom@gmail.com>
2022-12-14 20:00:05 +0700

    Login Lagi

---
+++ app/Filters/Auth.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Auth implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+
+        if (!session()->has('email_dosen')) {
+            // return redirect()->to("/");
+
+            return redirect()->to(base_url('/login'))->with('msg', 'Anda harus Login terlebih dahulu');
+        }
+        //return redirect()->to(base_url('/'));
+        // Do something here
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+      
+        // Do something here
+    }
+}
